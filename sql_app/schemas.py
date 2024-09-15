8890549891f6b938be9131d5e224258940baa87e
vitoriatanan <vitoriatanans@gmail.com>
2024-09-14 23:12:28 -0300

    feat: criação de esquemas do tipo Tarefa e atualização de dados

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,5 +1,20 @@
 from pydantic import BaseModel
 
+
+class TarefaBase(BaseModel):
+    description: str
+
+class TarefaCreate(TarefaBase):
+    pass
+
+class Tarefa(TarefaBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
 #Por segurança a senha não estará nos modelos pydantic, não será enviafo pel api ao ler um usuário.
 
 # classe Base definem os campos essenciais )atributos comuns) que todas as outras classes derivadas compartilham. São usadas para evitar repetição de código e para definir os atributos principais de Item e User
@@ -10,11 +25,13 @@ class UserBase(BaseModel):
     matricula: str
 
 
+
 # classes Create são usadas especcificamente para validação de entrada quando está criando novos objetos. Quando um novo usuário é criado, além do email, é necessário também o campo password, que não está em UserBase
 #As classes ItemCreate e UserCreate são separadas porque, ao criar novos objetos, os dados esperados são diferentes dos dados retornados (por exemplo, id não é fornecido ao criar, mas é necessário ao retornar o objeto).
 # Criação de usuário com senha
 class UserCreate(UserBase):
     password: str
+    is_adm: bool = False
 
 
 # Representação de saída e para a conversão de objetos de banco de dados em objetos Pydantic (via orm_mode)
@@ -22,24 +39,24 @@ class UserCreate(UserBase):
 # Retorno de usuário sem senha
 class User(UserBase):
     id: int #gerado automaticamete pelo banco de dados
-
+    tarefas: list[Tarefa] = []
+   
     class Config:
         orm_mode = True
 
 
-# Criação de administrador com senha
+'''/# Criação de administrador com senha
 class AdministradorCreate(UserBase):
     password: str
 
 # Retorno de administrador sem senha
 class Administrador(UserBase):
     id: int
-    owner_id: int
 
     class Config:
         orm_mode = True
 
-'''/
+
 RESUMINDO
 UserBase é apenas um molde para compartilhar campos comuns e economizar código.
 UserCreate é usada para receber e validar os dados quando um novo usuário é criado, incluindo a senha.
