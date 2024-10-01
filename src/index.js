6d5663a09c6fac7c13e1c1a7fc94e33cc5ca3dfa
vitoriatanan <vitoriatanans@gmail.com>
2024-09-30 23:31:35 -0300

    remoção

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import {createRoot } from 'react-dom/client'
-import React, { useEffect, useState } from "react";
-
-const root= createRoot(document.querySelector("#root"));
-
-root.render(<App/>)
-
-
-
